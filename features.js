70a9abb4849b08d03bf204ff09ecb8b92dcfe2e7
braydendiu <139385092+braydendiu@users.noreply.github.com>
2024-04-09 19:42:32 -0400

    Add files via upload

---
+++ features.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    const moonIcon = toggleButton.getElementsByClassName('fa-moon')[0];
+    const sunIcon = toggleButton.getElementsByClassName('fa-sun')[0];
+    
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+
+        moonIcon.classList.toggle('active');
+        sunIcon.classList.toggle('active');
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const typingSpan = document.getElementById('typing');
+    const text = 'Hi, I am Brayden'; 
+    let index = 0; 
+
+    function type() {
+        if (index < text.length) {
+            typingSpan.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 110); 
+        }
+    }
+
+    type(); 
+});

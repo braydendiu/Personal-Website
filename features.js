2fbaba5b26397dcf794e987eab997b434bf4cec9
braydendiu <139385092+braydendiu@users.noreply.github.com>
2024-04-15 12:17:35 -0400

    Add files via upload

--- features.js
+++ features.js
@@ -25,3 +25,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     type(); 
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navContainer = document.querySelector('.nav-container');
+
+    hamburger.addEventListener('click', () => {
+        navContainer.classList.toggle('active');
+    });
+});

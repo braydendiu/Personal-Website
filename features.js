b3a1a3a5cc12efe0088f9c3e6125dc00b463a398
Brayden Diu <139385092+braydendiu@users.noreply.github.com>
2024-10-16 00:48:45 -0400

    Add files via upload

--- features.js
+++ features.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleButton = document.getElementById('dark-mode-toggle');
-    const moonIcon = toggleButton.getElementsByClassName('fa-moon')[0];
-    const sunIcon = toggleButton.getElementsByClassName('fa-sun')[0];
+    const moonIcon = toggleButton.querySelector('.fa-moon');
+    const sunIcon = toggleButton.querySelector('.fa-sun');
     
     toggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
-
-        moonIcon.classList.toggle('active');
-        sunIcon.classList.toggle('active');
+        if (document.body.classList.contains('dark-mode')) {
+            moonIcon.style.display = 'none';
+            sunIcon.style.display = 'inline-block';
+        } else {
+            moonIcon.style.display = 'inline-block';
+            sunIcon.style.display = 'none';
+        }
     });
-});
-document.addEventListener('DOMContentLoaded', function() {
+
     const typingSpan = document.getElementById('typing');
     const text = 'Hey, I am Brayden'; 
     let index = 0; 
